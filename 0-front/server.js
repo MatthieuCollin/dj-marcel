161fc4e781b46287144dd65f1abaa44f85bdf656
Sachacouget <sacha.couget@viacesi.fr>
2025-12-02 14:36:13 +0100

    decoupage-front

---
+++ 0-front/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ---- PAGES ----
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get('/admin.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'admin.html')));
+app.get('/admin', (req, res) => res.sendFile(path.join(__dirname, 'views', 'admin.html')));
+app.get('/client.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'client.html')));
+app.get('/client', (req, res) => res.sendFile(path.join(__dirname, 'views', 'client.html')));
+app.get('/dj.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'dj.html')));
+app.get('/dj', (req, res) => res.sendFile(path.join(__dirname, 'views', 'dj.html')));
+
+app.listen(PORT, () => console.log(`DJ Marcel running at http://localhost:${PORT}`));
